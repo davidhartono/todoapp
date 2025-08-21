fc89dcd83327fe979ecdb5f7dec470b2f86461a2
davidhartono <davidhartono@gmail.com>
2025-08-21 23:31:44 +0700

    add site configuration

---
+++ config/site.tsx
@@ -0,0 +1,6 @@
+import { SiteConfig } from "@/interfaces/site-config";
+
+export const siteConfig: SiteConfig = {
+  name: "To Do List Application",
+  description: "A simple to-do list application built with Next.js",
+};
